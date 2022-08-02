716958cbbb53a9c6cdeb2724cc545868bbb1b3f8
ehlersmj4814 <mattehlers96@gmail.com>
2022-08-02 16:16:09 -0700

    mobileDeviceRename Script upload

--- mobileDeviceRename.zsh
+++ mobileDeviceRename.zsh
@@ -29,9 +29,11 @@
 ####################################################################################################
 
 # HARDCODED VALUES SET HERE
-jamfAPIURL=""
-apiUser=""
-apiPass=""
+jamfAPIURL="<JAMF URL HERE>"
+apiUser="<API USERNAME HERE>"
+apiPass="<API PW HERE>"
+csvPath=/tmp/devicenames.csv
+url="<GSHEET URL HERE>"
 
 ####################################################################################################
 # 
@@ -39,14 +41,38 @@ apiPass=""
 #
 ####################################################################################################
 
-#API GET list of devices from smart group
+#Removes previous downloaded csv
+if [ -e /tmp/devicenames.csv ]
+then
+	rm /tmp/devicenames.csv
+  echo "Removed previous CSV file."
+fi
 
+#Downloads current version of csv
+curl -L $url -o /tmp/devicenames.csv
+echo "CSV Download Complete"
 
-#Check list of devices against Google Sheet
+#API GET list of device SN's from smart group
+serialNumbers=$(curl -su $apiUser:$apiPass -H "accept: text/xml" $jamfAPIURL/mobiledevicegroups/id/60 -X GET | xmllint --format - | awk -F'>|<' '/serial_number/{print $3}')
 
-
-#API POST device name based on serial number
-
+#Verify Device(s) in the Jamf Smart Group
+if [[ -z "$serialNumbers" ]]; then
+    echo "No devices in the Jamf Rename Smart Group"
+else
+    while IFS= read -r sn; do
+        #Set Device Name and Asset Tag based off GSHEET values
+        deviceName=`(cat $csvPath | grep -i "$sn" | awk -F',' '{print $2}')`
+        assetTag=`(cat $csvPath | grep -i "$sn" | awk -F',' '{print $3}')`
+        #Verify device name(s) in GSHEET
+        if [[ -z "$deviceName" ]]; then
+            echo "No device name set in Asset Sheet"
+        else
+            #API PUT Device Name and Asset Tag based on Serial Number
+            echo "Setting" $sn "to" $deviceName "with asset" $assetTag
+            curl -su $apiUser:$apiPass -H "content-type: text/xml" $jamfAPIURL/mobiledevices/serialnumber/"$sn" -X PUT -d "<mobile_device><general><device_name>$deviceName</device_name><asset_tag>$assetTag</asset_tag></general></mobile_device>"
+        fi
+    done <<< "$serialNumbers"
+fi
 
 
 exit=0
